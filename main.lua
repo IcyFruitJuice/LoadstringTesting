c357a2c03339ccdd5987b559d2b2548c998d7952
IcyFruitJuice <nicholastnt111@gmail.com>
2024-05-21 17:59:36 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,9 @@
-while 1==1 do
-   wait(1)
-   print("this is working!")
-end
+local RarityPart = game.Workspace.RarityPart
+local ServerScriptService = game:GetService("ServerScriptService")
+local RarityHandler = require(ServerScriptService.MainRarityScript.RarityHandler)
+
+RarityPart.Color = RarityHandler.RarityData.PhysicalData.Diamond.Color
+RarityPart.Material = RarityHandler.RarityData.PhysicalData.Diamond.Material
+RarityPart.Transparency = RarityHandler.RarityData.PhysicalData.Diamond.Transparency
+RarityPart.BillboardGui.RarityLabel.Text = "Diamond"
+RarityPart.BillboardGui.TypeLabel.Text = RarityHandler.RarityData.PhysicalData.Diamond.RarityType
