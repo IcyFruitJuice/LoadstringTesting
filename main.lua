e8513d85ab5d756b843717e162630c6e688af6f3
IcyFruitJuice <nicholastnt111@gmail.com>
2024-05-21 18:02:37 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,8 +2,9 @@ local RarityPart = game.Workspace.RarityPart
 local ServerScriptService = game:GetService("ServerScriptService")
 local RarityHandler = require(ServerScriptService.MainRarityScript.RarityHandler)
 
-RarityPart.Color = RarityHandler.RarityData.PhysicalData.Diamond.Color
-RarityPart.Material = RarityHandler.RarityData.PhysicalData.Diamond.Material
-RarityPart.Transparency = RarityHandler.RarityData.PhysicalData.Diamond.Transparency
-RarityPart.BillboardGui.RarityLabel.Text = "Diamond"
-RarityPart.BillboardGui.TypeLabel.Text = RarityHandler.RarityData.PhysicalData.Diamond.RarityType
+RarityPart.Color = RarityHandler.RarityData.Unused.ForceField.Color
+RarityPart.Material = RarityHandler.RarityData.Unused.ForceField.Material
+RarityPart.Transparency = RarityHandler.RarityData.Unused.ForceField.Transparency
+RarityPart.BillboardGui.RarityLabel.Text = "ForceField"
+RarityPart.BillboardGui.RarityLabel.Color = RarityHandler.RarityData.Unused.ForceField.Color
+RarityPart.BillboardGui.TypeLabel.Text = RarityHandler.RarityData.Unused.ForceField.RarityType
